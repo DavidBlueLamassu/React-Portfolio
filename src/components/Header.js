61c8da1e368a4df39eaaede7738bd97b97fbd3f5
DavidBlueLamassu <dra21@cam.ac.uk>
2023-03-11 16:48:15 +0000

    index.js, App.js and page components added

---
+++ src/components/Header.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <NavLink
+          to="/"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Home
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="about"
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          About
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="portfolio"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Portfolio
+        </NavLink>
+      </li>
+      <li className="nav-item">
+        <NavLink
+          to="contact"
+          end
+          className={({ isActive }) =>
+            isActive ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Contact
+        </NavLink>
+      </li>
+    </ul>
+  );
+}
+
+export default Navbar;
