fe6bf531262f027cfc4618f2703de9d24835f59d
DavidBlueLamassu <dra21@cam.ac.uk>
2023-03-12 21:04:48 +0000

    About page removed Home and Contact modified

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,27 +1,77 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+const styles = {
+  page: {
+    background: "rgb(195, 219, 239)",
+    fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif"
+  },
+  header: {
+    fontSize: "50px",
+    textAlign: "center",
+    marginBottom: "50px",
+    paddingTop: "20px"
+  },
+  linkSpacing: {
+    marginBottom: "50px",
+  },
+  links: {
+    textDecoration: "none",
+    color: "black",
+    display: "flex",
+    justifyContent: "center"
+  },
+  textMe: {
+    fontSize: "30px",
+    marginBottom: "10px"
+  },
+  input: {
+    paddingBottom: "150px",
+    width: "300px",
+    display: "flex",
+    justifyContent: "center" 
+  },
+  inputSpacing: {
+    display: "flex",
+    justifyContent: "center"
+  },
+  button: {
+    marginBottom: "500px",
+    marginTop: "20px",
+    background: "rgb(63, 5, 139)",
+    border: "rgb(63, 5, 139)"
+  }
+};
 
 function Contact(props) {
   return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-      <Link to="contact" role="button" className="btn btn-link">
-        Message Me
-      </Link>
+    <div style={styles.page}>
+      <h1 style={styles.header}>Contact Me</h1>
+      <div className="row" style={styles.linkSpacing}>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <a href="https://github.com/DavidBlueLamassu" style={styles.links}>GitHub Profile</a>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <a href="https://www.linkedin.com/in/david-adams-922022a1/" style={styles.links}>LinkedIn Page</a>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <a href="mailto:dra21@cam.ac.uk" style={styles.links}>E-mail</a>
+        </div>
+        <div className="col-lg-3 col-md-6 col-sm-12">
+          <a href="https://github.com/DavidBlueLamassu" style={styles.links}>CV</a>
+        </div>
+      </div>
+      {/* Overview https://getbootstrap.com/docs/4.0/components/forms/ */}
+      <form>
+        <div className="form-group" style={styles.inputSpacing}>
+          <label style={styles.textMe}>Send me a text</label>
+        </div>
+        <div style={styles.inputSpacing}>
+          <input className="form-control"  placeholder="Message" style={styles.input}/>
+        </div>
+        <div style={styles.inputSpacing}>
+          <button type="submit" className="btn btn-primary" style={styles.button}>Submit</button>
+        </div>
+      </form>
   </div>
   );
 }
